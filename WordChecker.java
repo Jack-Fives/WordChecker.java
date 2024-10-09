7c922407e0563b6457f36a6b0bcfb4535e62b5de
Jack-Fives <jackf58@nycstudents.net>
2024-10-09 16:35:54 +0000

    first commit

---
+++ WordChecker.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+public class WordChecker {
+    private ArrayList<String> wordList;
+    public WordChecker(ArrayList<String> list)
+    {
+        wordList = list;
+    }
+    public WordChecker(){
+        wordList = new ArrayList<String>();
+    }
+    
+}
